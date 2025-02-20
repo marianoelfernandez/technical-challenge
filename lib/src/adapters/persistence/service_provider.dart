9762863ff23ac169b4baf72a01903cb1565f024f
marianoelfernandez <marianoelfr@gmail.com>
2025-02-19 22:12:09 -0300

    not saving pw

--- lib/src/adapters/persistence/service_provider.dart
+++ lib/src/adapters/persistence/service_provider.dart
@@ -1,7 +1,7 @@
-import "package:faithwave_app/src/adapters/persistence/auth_service_persistence.dart";
 import "package:faithwave_app/src/adapters/persistence/todo_service_persistence.dart";
-import "package:faithwave_app/src/features/auth/services/auth_service.dart";
 import "package:faithwave_app/src/features/todo/services/todo_service.dart";
+import "package:faithwave_app/src/adapters/mocks/auth_service_mock.dart";
+import "package:faithwave_app/src/features/auth/services/auth_service.dart";
 import "package:get_it/get_it.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
@@ -10,7 +10,7 @@ class PersistenceServiceProvider {
     final prefs = await SharedPreferences.getInstance();
 
     GetIt.I.registerSingleton<AuthService>(
-      AuthServicePersistence(prefs),
+      AuthServiceMock(),
     );
 
     GetIt.I.registerSingleton<TodoService>(
